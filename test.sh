e03718d1ea4e973ca3b753b527d82ad37e79d7fa
Francisco E. Paez <franpaez+github@gmail.com>
2021-04-06 19:46:16 -0300

    Verifica que los programas chequeen sus argumentos.

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+exec_result=false
+fork_result=false
+mfork_result=false
+
 if [ -f "bin/exec" ]; then
     printf "Testing: bin/exec\n"
 
@@ -45,25 +49,35 @@ if [ -f "bin/exec" ]; then
 
     if $exec1 && $exec2 && $exec3 && $exec4 ; then
         exec_result=true
-    else
-        exec_result=false
     fi
+else
+    printf "exec.c no esta compilado.\n"
 fi
 
 if [ -f "bin/fork" ]; then
     printf "Testing: bin/fork test.txt hola mundo\n"
 
-    bin/fork test.txt hola mundo > /dev/null
-    bin/fork test.txt hola mundo > /dev/null
+    printf "\tProbando que verifique argumentos: "
+    bin/fork > /dev/null 2>&1
+    if [ $(echo $?) != 0 ]; then
+        printf "Ok\n"
+        fork1=true
+    else
+        printf "Fallo\n"
+        fork1=false
+    fi
+
+    bin/fork test.txt hola mundo > /dev/null 2>&1
+    bin/fork test.txt hola mundo > /dev/null 2>&1
 
     printf "\tVerificando permisos del archivo test.txt: "
     perms=$(ls -lh test.txt | awk '{print $1}')
     if [ "$perms" = "-rw-r--r--" ]; then
         printf "Ok\n"
-        result1=true
+        fork2=true
     else
         printf "Fallo\n"
-        result1=false
+        fork2=false
     fi
 
     printf "\tVerificando contenido del archivo test.txt: "
@@ -72,32 +86,38 @@ if [ -f "bin/fork" ]; then
     if [ "$linea2" = "Hijo: mundo" ] && 
         [ "$linea1" = "Padre: hola" ]; then
         printf "Ok\n"
-        result2=true
+        fork3=true
     else
         printf "Fallo\n"
-        result2=false
+        fork3=false
     fi
 
     printf "\tVerificando el modo O_APPEND: "
     line_count=$(wc -l test.txt | awk '{print $1}')
     if [ "$line_count" = "4" ]; then
         printf "Ok\n"
-        result3=true
+        fork4=true
     else
         printf "Fallo\n"
-        result3=false
+        fork4=false
     fi
 
     rm test.txt
 
-    if $result1 && $result2 && $result3 ; then
+    if $fork1 && $fork2 && $fork3 && $fork4 ; then
         fork_result=true
-    else
-        fork_result=false
     fi
+else
+    printf "fork.c no esta compilado.\n"
 fi
 
-if $exec_result && $for_result; then
+if [ -f "bin/mfork" ]; then
+    printf "...\n"
+else
+    printf "mfork.c no esta compilado.\n"
+fi
+
+if $exec_result && $fork_result && $mfork_result; then
     exit 0
 else
     exit 1
