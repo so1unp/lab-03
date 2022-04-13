1214ef74e7ed05dc0673ccb5dce0d1c60c39a6f4
Francisco E. Paez <franpaez+github@gmail.com>
2022-04-13 11:52:52 -0300

    Reformat if clause.

--- sh.c
+++ sh.c
@@ -106,10 +106,12 @@ int main(void)
             // Clumsy but will have to do for now.
             // Chdir has no effect on the parent if run in the child.
             buf[strlen(buf) - 1] = 0;	// chop \n
-            if (chdir(buf + 3) < 0)
-            fprintf(stderr, "cannot cd %s\n", buf + 3);
+            if (chdir(buf + 3) < 0) {
+                fprintf(stderr, "cannot cd %s\n", buf + 3);
+            }
             continue;
         }
+
         if (fork1() == 0) {
             runcmd(parsecmd(buf));
         }
